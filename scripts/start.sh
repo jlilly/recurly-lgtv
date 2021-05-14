670380bd8f3d2ada0828860a937b9b433c68004e
Joe Lilly <joseph.lilly@starz.com>
2021-05-14 17:10:28 -0600

    do all the things on `npm start`

---
+++ scripts/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start the dev server
+ng serve &
+trap 'kill $! && exit 0' EXIT
+
+# Wait a bit for the build to stop spewing output on the console
+sleep 10
+
+# Check to see if we need to generate `webos/index.html` or the ipk file
+(ls webos/index.html 1>/dev/null 2>&1 && ls webos/*.ipk 1>/dev/null 2>&1) || scripts/create-lgtv-files.js 
+
+# Launch the app on the tv (has built in selector)
+scripts/install-and-launch.js 
+
+# Pretend the dev server is in the foreground
+wait
